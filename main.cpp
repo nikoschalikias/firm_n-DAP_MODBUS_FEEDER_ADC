a100546878bcfd823650ba41e34263c8968dd742
nikos chalikias <nikos.chalikias@gmail.com>
2022-12-13 17:20:42 +0000

    fw project replaced with download from keil-studio

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+// 2020
+// PROJECT MOVED TO https://github.com/nikoschalikias/firm_n-DAP_MODBUS_FEEDER_ADC
+// USING mbed-cli
+// --------------------------------------
+// 13/12/2022
+    // made a new os2 project in keil-studio
+    // Updated main.cpp from  this https://ide.mbed.com/compiler/#nav:/n-DAP_MODBUS_FEEDER_ADC;
+    // Updated mbed.bld with this content: https://os.mbed.com/users/mbed_official/code/mbed/builds/176b8275d35d
+    // Updated  the SDK with CTRL+SHIFT+P: Refresh mbed2 SDK
+    // Imported the USBSerial from version 71:53949e6131f6 at https://os.mbed.com/users/mbed_official/code/USBDevice/
+    // ...then had a single error
+    // I then  removed line 24 from USBHAL.h and 
+    // Compiled
+    // To be tested if it works
+    // printf() over USB works :-)
+    // All updates form  main.cpp from: https://github.com/nikoschalikias/firm_n-DAP_MODBUS_FEEDER_ADC/blob/master/main.cpp ,  copied here
+    // USB serial works, red LED blinks
+
 //--------------N-DAP BOARD PINOUT----------------------------
 // RX       P0.19           P0.22(ADC0)     PRESSURE SENSOR 
 // TX       P0.18           P0.8            RC-SERVO
@@ -5,20 +23,20 @@
 //          VBUS            P0.12(ADC1)
 //          3.3V            P0.5(SDA)
 //          GND             P0.4(SCL)
-//------------------------------------------------------------
+//------------------------------------------------------------    
 
 #include "mbed.h"
 #include "LPC11Uxx.h"
 #include "USBSerial.h"
 //#include "Servo.h"
 
-DigitalOut red(P0_20);                              //n-DAP on board LEDs
+DigitalOut red(P0_20);
 DigitalOut green(P0_21);
 DigitalOut blue(P0_11);
 
-AnalogIn adc1(P0_22);   	                        //n-DAP pin 12
-USBSerial serial(0x1f00,0x2012,0x0001,false);       // serial over native USB on board (false is non blocking)
-Serial modbus(P0_19, P0_18);                        // n-DAP tx, rx
+AnalogIn adc1(P0_22); //n-DAP pin 12
+USBSerial serial(0x1f00,0x2012,0x0001,false);// serial over native USB on board (false is non blocking)
+Serial modbus(P0_19, P0_18); // tx, rx
 DigitalOut dir_RS485(P0_2);
 PwmOut rcservo(P0_8);
 DigitalOut Vacuum(P0_7);
@@ -27,17 +45,17 @@ int main() {
     modbus.baud(9600);
     uint8_t buf[32];
     uint8_t i = 0;
-    uint16_t angle = 0;
     uint16_t counter = 0;
+    uint16_t angle = 0;
+
     dir_RS485 = 1;
-    
-    wait(0.5);                                      // wait the usb to be recognized from the PC
+    wait(0.5); // wait the usb to be recognized from the PC
     modbus.printf(" 0x%04u\r",  adc1.read_u16());
-    serial.printf(" 0x%04u\r",  adc1.read_u16());   //adc readout to PC  
+    serial.printf("ADC =  0x%04u\n\r",  adc1.read_u16());   //adc readout to PC  
+    //serial.printf("HI FERNANDO");   //adc readout to PC  
     dir_RS485 = 0;
     green = 1;
     blue = 1;
-    red = 1;
     rcservo.period_ms(20);      
     rcservo.pulsewidth_us(1500) ;
        
@@ -46,17 +64,19 @@ int main() {
         if (counter > 20000) {
             red = !red;
             counter = 0; 
-        }          
+        }  
+
+
         if(modbus.readable()) {                                                 
             if (i > 31) {                                
                 i = 0;
                 }
             buf[i] = modbus.getc();
             if ( buf[i] == 0x20){
-                  serial.printf("\n");              //change line and reset index
+                  serial.printf("\n");    //change line and reset index
                   i = 0; buf[i] = 0x20;
                   }
-            modbus.putc(buf[i]);                    //debug only
+            modbus.putc(buf[i]);                            //debug only
             serial.printf("0x%02X ", buf[i]);
             i++; 
         }
@@ -73,6 +93,6 @@ int main() {
             rcservo.period_ms(20);      
             rcservo.pulsewidth_us(angle) ;
             }
-
     }
 }
+
---
+++ tasks.md
@@ -0,0 +1,21 @@
+- [x] 1. Review source, understand what it does
+- [x] 2. Create README.MD and describe firmware operation
+- [x] 3. Document the structure of the MODBUS messages
+- [x] 4. Add Vacuum GPO
+- [x] 5. LED and Vacuum commands (values 1 to 5) dont affect Servo
+- [x] 6. [IS NOT] Check if its possible to change from mbed OS2 to OS5
+- [x] 7. Create mbed cli project and compile it
+- [x] 8. Add repo to sourcetree
+- [x] 9. Create github remote repo and push
+- [x] 10. Lower heart-beat red led frequency so blink is visible
+- [x] 11. Test mbed-cli created .bin lower heart-beat
+- [x] 12. tidy-up main() comments
+- [x] 13. reduce startup delay from 4 sec to 0.5 sec
+- [x] 14. Create and test dos-cmd file to upload .bin to target
+- [x] 15. Update README.MD with old/new project remote repo URLs
+- [x] 16. Test Vacuum GPO line with LED, controlled from modbus
+- [x] 17. Create in Wiki instructions how to inport mbed OS2 program to mbed-cli, save a snapshot to doc folder
+- [x] 18. Add Schematic info to main.cpp
+- [x] 19. Add Schematic info to README.MD
+- [x] 20. Create Block Diagram
+- [ ] 21. Tidy-Up README.MD title
